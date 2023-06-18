603cb1586056097b0edf36de3c1d2eb0450ecdf6
Krzysztof Nowak <krzyqq@gmail.com>
2023-06-18 17:54:29 +0200

    renamed Main class to JobOffersSpringBootApplication

--- src/main/java/pl/joboffers/JobOffersSpringBootApplication.java
+++ src/main/java/pl/joboffers/JobOffersSpringBootApplication.java
@@ -1,6 +1,6 @@
 package pl.joboffers;
 
-public class Main {
+public class JobOffersSpringBootApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
