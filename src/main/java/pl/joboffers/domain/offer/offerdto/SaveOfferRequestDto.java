61c4e7b289f2b4a9b908fd87812b6cfd764ceabf
Krzysztof Nowak <krzyqq@gmail.com>
2023-07-20 10:46:41 +0200

    added validationMessages.properties

--- src/main/java/pl/joboffers/domain/offer/offerdto/SaveOfferRequestDto.java
+++ src/main/java/pl/joboffers/domain/offer/offerdto/SaveOfferRequestDto.java
@@ -2,21 +2,22 @@ package pl.joboffers.domain.offer.offerdto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Builder
 public record SaveOfferRequestDto(
-        @NotNull(message = "is empty")
-        @NotNull(message = "id is null")
+        @NotNull(message = "{offerUrl.not.null}")
+        @NotEmpty(message = "{offerUrl.not.empty}")
         String offerUrl,
-        @NotNull(message = "is empty")
-        @NotNull(message = "is null")
+        @NotNull(message = "{jobPosition.not.null}")
+        @NotEmpty(message = "{jobPosition.not.empty}")
         String jobPosition,
-        @NotNull(message = "is empty")
-        @NotNull(message = "is null")
+        @NotNull(message = "{companyName.not.null}")
+        @NotEmpty(message = "{companyName.not.empty}")
         String companyName,
-        @NotNull(message = "earnings must not be empty")
-        @NotNull(message = "earnings must not be null")
+        @NotNull(message = "{earnings.not.null}")
+        @NotEmpty(message = "{earnings.not.empty}")
         String earnings )
 {
 }
